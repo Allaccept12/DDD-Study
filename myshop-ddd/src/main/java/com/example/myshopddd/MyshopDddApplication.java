4434707ad4a40aafb1d2232c5545249c227b5064
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-05-11 19:09:29 +0900

    ddd study project init

---
+++ myshop-ddd/src/main/java/com/example/myshopddd/MyshopDddApplication.java
@@ -0,0 +1,13 @@
+package com.example.myshopddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshopDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshopDddApplication.class, args);
+	}
+
+}
---
+++ myshop-ddd/src/test/java/com/example/myshopddd/MyshopDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myshopddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyshopDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
